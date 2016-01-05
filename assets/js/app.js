3964924741ea2b62fc139e44b03609920fec5c93
c5fritz1387 <charles.e.fritz@gmail.com>
2016-01-04 22:40:53 -0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -106,12 +106,7 @@ var properties = [{
     sortable: true
   },
   filter: {
-    type: "integer",
-    input: "checkbox",
-    vertical: true,
-    multiple: true,
-    operators: ["in", "not_in", "equal", "not_equal"],
-    values: []
+    type: "integer"
   }
 },
 {
@@ -122,12 +117,7 @@ var properties = [{
     sortable: true
   },
   filter: {
-    type: "integer",
-    input: "checkbox",
-    vertical: true,
-    multiple: true,
-    operators: ["in", "not_in", "equal", "not_equal"],
-    values: []
+    type: "integer"
   }
 },
 {
@@ -138,13 +128,7 @@ var properties = [{
     sortable: true
   },
   filter: {
-    type: "integer",
-    input: "checkbox",
-    vertical: true,
-    multiple: true,
-    operators: ["in", "not_in", "equal", "not_equal"],
-    values: []
-  }
+    type: "integer"
 },
 {
   value: "Access1km",
